5d3bd87b7aa7830cd78be267e94ec412a338755f
Harri Hätinen <hhatinen@gmail.com>
2022-07-21 08:35:34 +0300

    add test test

---
+++ NumberClassifier/src/test/java/NumberClassifierTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NumberClassifierTest {
+
+    @Test
+    void testHello()
+    {
+        assertEquals(2, 1, "Hello world");
+    }
+
+}
