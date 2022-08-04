332cdb8189af061ce1244804bbb78f1f05bd486d
Harri Hätinen <hhatinen@gmail.com>
2022-08-04 20:04:37 +0300

    refactor training code to NeuralNetworkTrainer. write documentation

--- NumberClassifier/app/src/main/java/NumberClassifier/App.java
+++ NumberClassifier/app/src/main/java/NumberClassifier/App.java
@@ -17,78 +17,24 @@ public class App {
             
             File file = new File("test-conf.json");
             TrainConfig conf = TrainConfig.loadJSON(new Scanner(file).useDelimiter("\\Z").next());
+            NeuralNetworkTrainer trainer = new NeuralNetworkTrainer(conf);
             
             ImageSet imageSet = ImageSet.loadFromMNIST(
                     new FileInputStream(new File(conf.trainingData)),
                     new FileInputStream(new File(conf.trainingLabels)),
                     10);
-            
-            FeedForwardNeuralNetwork nn = new FeedForwardNeuralNetwork( conf.activationFunction, conf.layers );
-            nn.randomizeWeights(conf.initWeights[0], conf.initWeights[1]);
-            nn.setBiases(conf.initBiases);
-    
             TrainingExample[] trainingExamples = imageSet.createTrainingExamples();
-            
-            for ( int i = 0; i < conf.epochs; i++ ) {
-                int[] batchIndices = new int[conf.miniBatchSize];
-                for ( int j = 0; j < conf.miniBatchSize; j++ ) {                    
-                    batchIndices[j] = (int) (Math.random() * trainingExamples.length);
-                    boolean isDuplicate = false;
-                    for( int k = 0; k < j; k++ ) {
-                        if( batchIndices[k] == batchIndices[j] ) {
-                            isDuplicate = true;
-                            break;
-                        }
-                    }
-                    if ( isDuplicate ) {
-                        j--;
-                    }
-                }
+            trainer.train(trainingExamples);
 
-                TrainingExample[] examples = new TrainingExample[conf.miniBatchSize];
-                for ( int j = 0; j < conf.miniBatchSize; j++ ) {
-                    examples[j] = trainingExamples[batchIndices[j]];
-                }
-
-                //double C0 = nn.calculateCost(examples);
-                //System.out.println( "Epoch " + i + " before: " + C0 );
-
-                nn.trainEpoch(examples, conf.learningRate);
-
-                if ( i % 100 == 0 )
-                {
-                    double C1 = nn.calculateCost(examples);
-                    System.out.println( "Epoch " + i + " after: " + C1 );
-                }
-            }
-
-                
             ImageSet testSet = ImageSet.loadFromMNIST(
                 new FileInputStream(new File(conf.testData)),
                 new FileInputStream(new File(conf.testLabels)),
                 10);
             TrainingExample[] testExamples = testSet.createTrainingExamples();
+            double accuracy = trainer.testAccuracy(testExamples);
 
-            int correct = 0;
-            for ( int i = 0; i < testExamples.length; i++ ) {
-                int result = nn.getMaxActivation(testExamples[i].input);
-
-                int exampleResult = 0;
-                double max = testExamples[i].output[0];
-                for ( int j = 1; j < 10; j++ ) {
-                    if ( testExamples[i].output[j] > max ) {
-                        max = testExamples[i].output[j];
-                        exampleResult = j;
-                    }
-                }
-
-                if ( result == exampleResult )
-                    correct++;
-            }
-
-            System.out.println("Accuracy: " + ((double)correct / testExamples.length) * 100.0 + "%" );
+            System.out.println("Accuracy: " + accuracy * 100.0 + "%" );
             
-
         } catch (Exception e) {
             e.printStackTrace();
         }        
---
+++ NumberClassifier/app/src/main/java/NumberClassifier/NeuralNetworkTrainer.java
@@ -0,0 +1,93 @@
+package NumberClassifier;
+
+/**
+ * Implements training strategy for FeedForwardNeuralNetwork. This includes dividing the training data into mini-batches and measuring the network accuracy.
+ */
+public class NeuralNetworkTrainer {
+
+    /**
+     * Construct NeuralNetworkTrainer with the given configuration.
+     * @param trainConfig The configuration to use.
+     * @throws Exception
+     */
+    public NeuralNetworkTrainer(TrainConfig trainConfig) throws Exception {
+        this.trainConfig = trainConfig;
+
+        this.nn = new FeedForwardNeuralNetwork( trainConfig.activationFunction, trainConfig.layers );
+        nn.randomizeWeights(trainConfig.initWeights[0], trainConfig.initWeights[1]);
+        nn.setBiases(trainConfig.initBiases);
+    }
+
+    /**
+     * Train the neural network.
+     * @param trainingExamples Training examples to use.
+     * @throws Exception
+     */
+    public void train(TrainingExample[] trainingExamples) throws Exception {
+       
+        for ( int i = 0; i < trainConfig.epochs; i++ ) {
+            TrainingExample[] batch = pickMiniBatch(trainingExamples, trainConfig.miniBatchSize);
+
+            nn.trainEpoch(batch, trainConfig.learningRate);
+
+            if ( i % 100 == 0 ) {
+                double C1 = nn.calculateCost(trainingExamples);
+                System.out.println( "Epoch " + i + " after: " + C1 );
+            }
+        }        
+    }
+
+    /**
+     * Mesaure accuracy of the neural network. Assumes that the network has already been trained.
+     * @param testExamples Test examples to use.
+     * @return Accuracy of the network, a value between 0-1.
+     * @throws Exception
+     */
+    double testAccuracy(TrainingExample[] testExamples) throws Exception {
+        int correct = 0;
+        for ( int i = 0; i < testExamples.length; i++ ) {
+            int result = nn.getMaxActivation(testExamples[i].input);
+
+            int exampleResult = 0;
+            double max = testExamples[i].output[0];
+            for ( int j = 1; j < 10; j++ ) {
+                if ( testExamples[i].output[j] > max ) {
+                    max = testExamples[i].output[j];
+                    exampleResult = j;
+                }
+            }
+
+            if ( result == exampleResult )
+                correct++;
+        }
+
+        return (double)correct / testExamples.length;
+    }
+
+    private TrainingExample[] pickMiniBatch( TrainingExample[] trainingExamples, int batchSize ) {
+        int[] batchIndices = new int[trainConfig.miniBatchSize];
+        for ( int j = 0; j < trainConfig.miniBatchSize; j++ ) {                    
+            batchIndices[j] = (int) (Math.random() * trainingExamples.length);
+            boolean isDuplicate = false;
+            for( int k = 0; k < j; k++ ) {
+                if( batchIndices[k] == batchIndices[j] ) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if ( isDuplicate ) {
+                j--;
+            }
+        }
+
+        TrainingExample[] examples = new TrainingExample[trainConfig.miniBatchSize];
+        for ( int j = 0; j < trainConfig.miniBatchSize; j++ ) {
+            examples[j] = trainingExamples[batchIndices[j]];
+        }
+
+        return examples;
+    }
+
+    private FeedForwardNeuralNetwork nn;
+    private TrainConfig trainConfig;
+}
